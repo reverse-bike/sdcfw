98c8f589b181e73f7c86a51fd4ae0e53c1b42f1b
Bo Lopker <lopkerk@gmail.com>
2026-01-06 12:29:59 -0800

    Add note about bluetooth

--- apps/web/src/components/GuidedTutorial.tsx
+++ apps/web/src/components/GuidedTutorial.tsx
@@ -268,6 +268,20 @@ export default function GuidedTutorial() {
 						</div>
 					</div>
 				</div>
+				<div class="bg-blue-50 border-l-4 border-blue-400 p-4 mb-4">
+					<div class="flex">
+						<div class="shrink-0">
+							<svg class="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+							</svg>
+						</div>
+						<div class="ml-3">
+							<p class="text-sm text-blue-700">
+								<strong>Note:</strong> After restoring firmware, you may need to delete the bike from your phone's Bluetooth list of known devices before it will connect again. Go to your phone's Bluetooth settings, find the bike, and "Forget" or remove it, then pair again.
+							</p>
+						</div>
+					</div>
+				</div>
 				<Restore selectedDevice={selectedDevice()} lastBackup={lastBackup()} inline={true} />
 			</ToolStep>
 		</div>
